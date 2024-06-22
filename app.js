8cf3b6d3ee9e32fdb7dff965074faa506c66bd41
Tolper4161 <arseniy.z2006@gmail.com>
2024-06-22 16:10:33 +0300

    Cors

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 const port = 8000;
 
 app.use(express.json());
+app.use(cors());
 
 app.get("/", (req, res) => {
     res.send("Hello world!");
